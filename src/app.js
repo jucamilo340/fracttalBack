9b6f0340dca39e67ccf5efce6638fb0dc00bac10
jucamilo340 <jucamilo340@gmail.com>
2023-07-23 23:02:00 -0500

    validations errors

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import dotenv from "dotenv";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import employeeRoutes from "./routes/employees.routes.js";
+import { FRONTEND_URL } from "./config.js";
 
 const app = express();
 dotenv.config();
 app.use(
     cors({
       credentials: true,
-      origin: "*",
+      origin: FRONTEND_URL,
     })
   );
   app.use(express.json());
